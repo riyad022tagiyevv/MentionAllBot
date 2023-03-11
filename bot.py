f6d996bab254a32ec81ec2b60516f4650112539d
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2023-03-11 15:30:30 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,8 +25,8 @@ async def start(event):
     link_preview=False,
     buttons=(
       [
-        Button.url('📣 Channel', 'https://t.me/harp_tech'),
-        Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
+        Button.url('📣 Channel', 'https://t.me/OldHumans'),
+        Button.url('📦 Source', 'https://github.com/riyad022/MentionAllBot')
       ]
     )
   )
@@ -39,8 +39,8 @@ async def help(event):
     link_preview=False,
     buttons=(
       [
-        Button.url('📣 Channel', 'https://t.me/harp_tech'),
-        Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
+        Button.url('📣 Channel', 'https://t.me/OldHumans'),
+        Button.url('📦 Source', 'https://github.com/riyad022/MentionAllBot')
       ]
     )
   )
